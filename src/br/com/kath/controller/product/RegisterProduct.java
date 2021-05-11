5ef547361218e037dd3eb81702190653f12ebf34
Cauã <cauakathdev@gmail.com>
2021-05-11 16:51:49 -0300

    Registro de produtos no banco

--- src/br/com/kath/controller/product/RegisterProduct.java
+++ src/br/com/kath/controller/product/RegisterProduct.java
@@ -1,12 +1,20 @@
 package br.com.kath.controller.product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
+import br.com.dao.DataBaseConnection;
 import br.com.kath.model.ProdutoModel;
 
 public class RegisterProduct {
 	
 	private Scanner input = new Scanner(System.in);
+	private Connection connection;
+	
+	public RegisterProduct() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
 
 	public ProdutoModel registerProduct() {
 		var productModel = new ProdutoModel();
@@ -20,6 +28,22 @@ public class RegisterProduct {
 		productModel.setProductQuantity(input.nextInt());
 		productModel.setStorageBalance(productModel.getProductQuantity() * productModel.getProductPrice());
 
+		try {
+			String sql = "INSERT INTO produto (productName, productPrice, productQuantity, storageBalance)"
+					+ "VALUES(?, ?, ?, ?)";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setString(1, productModel.getProductName());
+			preparedStatement.setDouble(2, productModel.getProductPrice());
+			preparedStatement.setInt(3, productModel.getProductQuantity());
+			preparedStatement.setDouble(4, productModel.getStorageBalance());
+			
+			preparedStatement.execute();
+			
+		} catch (Exception e) {
+			System.out.println("\nErro ao cadastrar os dados.");
+		}
+		
 		return productModel;
 	}
 	
