fdcdefaeaaa051396755e020809cf96abe22372b
Cauã <cauakathdev@gmail.com>
2021-05-11 16:50:29 -0300

    Configuração de conexão com o Banco de Dados

---
+++ src/br/com/dao/DataBaseConnection.java
@@ -0,0 +1,35 @@
+package br.com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseConnection {
+
+	private static DataBaseConnection dataBaseConnection;
+	private Connection connection;
+	
+	private DataBaseConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/"+
+					"loja?user=root&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+			);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("\nNão foi possível conectar no Banco de dados!");
+		}
+	}
+	
+	public static DataBaseConnection getInstance() {
+		if (dataBaseConnection == null) dataBaseConnection = new DataBaseConnection();
+		
+		return dataBaseConnection;
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	
+}
