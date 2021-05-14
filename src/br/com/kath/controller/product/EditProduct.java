5115d6d11fa79b07b0afefbb1db05dc90199a4d1
Cauã <cauakathdev@gmail.com>
2021-05-14 10:50:54 -0300

    Refatoração para editar dados cadastrados no BD

--- src/br/com/kath/controller/product/EditProduct.java
+++ src/br/com/kath/controller/product/EditProduct.java
@@ -1,102 +1,159 @@
 package br.com.kath.controller.product;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
+import br.com.dao.DataBaseConnection;
 import br.com.kath.model.ProdutoModel;
 
 public class EditProduct {
 	
 	private Scanner input = new Scanner(System.in);
+	private Connection connection;
+	private ProdutoModel product = new ProdutoModel();
 
-	public void editProduct(List<ProdutoModel> products) {
+	public EditProduct() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
+	
+	public ProdutoModel editProduct() {
+		PreparedStatement preparedStatement;
+		
 		StorageList storageList = new StorageList();
 		int productId, fieldIndex;
 		
-		if (products.size() <= 0) {
-			System.out.println("\nNão existem produtos cadastrados");
-			return;
+		if (storageList.listData() == null) {
+			return null;
 		}
-		
-		storageList.listData();
 			
 		System.out.println("\n---- EDITAR DADOS DE PRODUTO ----\n");
-		System.out.print("\nInforme o Id do produto: ");
-		productId = input.nextInt() - 1;
-
-		if (productId > products.size()) {
-			System.out.println("\nEsse produto não existe!");
-			return;
+		System.out.print("Informe o Id do produto: ");
+		productId = input.nextInt();
+		
+		try {
+			String sql = "SELECT * FROM produto WHERE cod = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setInt(1, productId);
+			
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if (!resultSet.next()) {
+				System.out.println("\nEste produto não existe");
+				return null;
+				
+			} else {
+				product.setProductName(resultSet.getString("productName"));
+				product.setProductPrice(resultSet.getDouble("productPrice"));
+				product.setProductQuantity(resultSet.getInt("productQuantity"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 		
-		System.out.print("---- CAMPOS ----" + 
+		System.out.print("\n---- CAMPOS ----\n" + 
 				"\n1) Nome do produto" + 
 				"\n2) Preço do produto" + 
-				"\n3) Quantidade do produto" + 
+				"\n3) Quantidade do produto \n" + 
 				"\nInforme o campo que deseja editar: ");
 		fieldIndex = input.nextInt();
 		
 		switch (fieldIndex) {
 		case 1:
-			editProductName(products, productId);
-			
+			editProductName(productId);
 			break;
 		
 		case 2:
-			editProductPrice(products, productId);
-			
+			editProductPrice(productId);
 			break;
 		
 		case 3:
-			editProductQuantity(products, productId);
-			
+			editProductQuantity(productId);
 			break;
 		}
 
-		return;
+		return product;
 	}
 	
-	private ProdutoModel editProductName(List<ProdutoModel> products, int productId) {
-		var product = new ProdutoModel();
+	private ProdutoModel editProductName(int productId) {
+		PreparedStatement preparedStatement;
 		
 		System.out.print("\nInforme o novo nome do produto: ");
 		product.setProductName(input.next());
 		
-		product.setProductPrice(products.get(productId).getProductPrice());
-		product.setProductQuantity(products.get(productId).getProductQuantity());
-		product.setStorageBalance(products.get(productId).getStorageBalance());
-		
-		products.set(productId, product);
+		try {
+			String sql = "UPDATE produto SET productName = ? WHERE cod = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setString(1, product.getProductName());
+			preparedStatement.setInt(2, productId);
+			
+			preparedStatement.execute();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 		
 		return product;
 	}
 	
-	private ProdutoModel editProductPrice(List<ProdutoModel> products, int productId) {
-		var product = new ProdutoModel();
+	private ProdutoModel editProductPrice(int productId) {
+		PreparedStatement preparedStatement;
 		
 		System.out.print("\nInforme o novo preço do produto: ");
 		product.setProductPrice(input.nextDouble());
 		
-		product.setProductName(products.get(productId).getProductName());
-		product.setProductQuantity(products.get(productId).getProductQuantity());
-		product.setStorageBalance(product.getProductPrice() * products.get(productId).getProductQuantity());
+		product.setStorageBalance(product.getProductPrice() * 
+				product.getProductQuantity());
 		
-		products.set(productId, product);
+		try {
+			String sql = "UPDATE produto SET productPrice = ?, storageBalance = ? "
+					+ " WHERE cod = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setDouble(1, product.getProductPrice());
+			preparedStatement.setDouble(2, product.getStorageBalance());
+			preparedStatement.setInt(3, productId);
+			
+			preparedStatement.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 		
 		return product;
 	}
 	
-	private ProdutoModel editProductQuantity(List<ProdutoModel> products, int productId) {
-		var product = new ProdutoModel();
+	private ProdutoModel editProductQuantity(int productId) {
+		PreparedStatement preparedStatement;
 		
 		System.out.print("\nInforme a nova quantidade do produto: ");
 		product.setProductQuantity(input.nextInt());
 		
-		product.setProductName(products.get(productId).getProductName());
-		product.setProductPrice(products.get(productId).getProductPrice());
-		product.setStorageBalance(product.getProductQuantity() * products.get(productId).getProductPrice());
+		product.setStorageBalance(product.getProductPrice() * 
+				product.getProductQuantity());
 		
-		products.set(productId, product);
+		try {
+			String sql = "UPDATE produto SET productQuantity = ?, storageBalance = ? "
+					+ " WHERE cod = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setInt(1, product.getProductQuantity());
+			preparedStatement.setDouble(2, product.getStorageBalance());
+			preparedStatement.setInt(3, productId);
+			
+			preparedStatement.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 		
 		return product;
 	}
