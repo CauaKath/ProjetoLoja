4a4b1b98740050225bb15c107cab9d418739e458
Cauã <cauakathdev@gmail.com>
2021-05-20 07:45:45 -0300

    Refatoração para remover itens do carrinho no BD

--- src/br/com/kath/controller/car/RemoveProductOutCar.java
+++ src/br/com/kath/controller/car/RemoveProductOutCar.java
@@ -1,34 +1,73 @@
 package br.com.kath.controller.car;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-import br.com.kath.model.PersonModel;
+import br.com.dao.DataBaseConnection;
 
 public class RemoveProductOutCar {
 	
 	private Scanner input = new Scanner(System.in);
+	private Connection connection;
+	
+	public RemoveProductOutCar() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
 
-	public void removeProductOutCar(PersonModel person) {
+	public void removeProductOutCar(int clientId) {
+		PreparedStatement preparedStatement;
 		int productId;
-		String productName;
 		var carProductList = new CarProductList();
-		var nameValidate = new NameValidate();
 		
-		carProductList.carProductsList(person);
-		
-		System.out.print("\nInforme o nome do produto que deseja remover do carrinho: ");
-		productName = input.next();
-		
-		productId = nameValidate.nameValidate(person.getCar().getCarProducts(), productName);
-		
-		if (productId == -1) {
-			System.out.println("\nProduto não existe no carrinho!");
+		if (carProductList.carProductsList(clientId) == false) {
 			return;
 		}
 		
-		person.getCar().getCarProducts().remove(productId);
+		System.out.print("\nInforme o ID do produto que deseja remover do carrinho: ");
+		productId = input.nextInt();
 		
-		return;
+		try {
+			
+			if (!this.checkIfProductExists(productId)) {
+				return;
+			}
+			
+			String sql = "DELETE FROM shopping_carts WHERE productId = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setInt(1, productId);
+			
+			preparedStatement.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+	
+	private boolean checkIfProductExists(int productId) {
+		PreparedStatement preparedStatement;
+		
+		try {
+			String sql = "SELECT * FROM products WHERE cod = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setInt(1, productId);
+			
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if (!resultSet.next()) {
+				System.out.println("\nEste produto não existe");
+				return false;
+			} else {
+				return true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 }
