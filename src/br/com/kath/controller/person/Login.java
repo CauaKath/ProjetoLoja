bae9a6c74fc6a9ff8e4a19109e76ba4430fcd4e3
Cauã <cauakathdev@gmail.com>
2021-05-20 07:46:21 -0300

    Refatoração para validar login pela tabela clients no BD

--- src/br/com/kath/controller/person/Login.java
+++ src/br/com/kath/controller/person/Login.java
@@ -1,40 +1,62 @@
 package br.com.kath.controller.person;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-import br.com.kath.model.PersonModel;
+import br.com.dao.DataBaseConnection;
 
 public class Login {
 
 	private Scanner input = new Scanner(System.in);
+	private Connection connection;
 	
-	public PersonModel login(List<PersonModel> people) {
+	public Login() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
+	
+	public int login() {
 		String name;
-		int id;
+		int clientId;
 		
-		System.out.println("\n--- CADASTRO ---\n");
+		System.out.println("\n--- Login ---\n");
 		System.out.println("Informe o seu nome: ");
 		name = input.next();
 		
-		id = this.validateLogin(people, name);
+		clientId = this.validateLogin(name);
 		
-		if (id == -1) {
+		if (clientId == -1) {
 			System.out.println("\nLogin inválido!");
-			return null;
+			return -1;
 		} else {
-			System.out.println("\nBem vindo a loja, " + people.get(id).getNome());
-			return people.get(id);
+			System.out.println("\nBem vindo a loja, " + name);
+			return clientId;
 		}
 	}
 	
-	public int validateLogin(List<PersonModel> people, String name) {
-		for (int i = 0; i < people.size(); i++) {
-			if (people.get(i).getNome().equals(name)) {
-				return i;
+	public int validateLogin(String name) {
+		PreparedStatement preparedStatement;
+		
+		try {
+			String sql = "SELECT * FROM clients WHERE clientName = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setString(1, name);
+			
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if (!resultSet.next()) {
+				return -1;
+			} else {
+				return resultSet.getInt("id");
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
 		}
-		return -1;
 	}
+	
+	
 	
 }
